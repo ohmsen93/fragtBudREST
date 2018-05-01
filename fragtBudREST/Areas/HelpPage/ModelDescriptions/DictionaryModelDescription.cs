b4ca6013cc0bc8dc9480935629970bc70899466a
Mads Ohmsen <Mads Ohmsen>
2018-05-01 18:09:45 +0200

    Add project files.

---
+++ fragtBudREST/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace fragtBudREST.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ fragtBudREST/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace fragtBudREST.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
